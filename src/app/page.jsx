13dabe312ce5061c26d1db71552a635a2412e14a
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-09-11 22:26:47 -0300

    body

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,25 +6,26 @@ import styles from "./page.module.css"
 export default function Home() {
   return (
     <>
-      <Nav />
-      <container className={styles.container}>
-        <video className={styles.video} src="/video.mp4"
-          autoPlay
-          loop
-          muted
-        >
-        </video>
-        <header className={styles.header}>
-          <h1 className={styles.title}>Elegância à Mesa <br /> Sabores que Encantam</h1>
-          <div>
-            <button className={styles.btn1}>Peça já</button>
-            <button className={styles.btn2}>Contate nos</button>
-          </div>
-        </header>
-      </container>
-
-      <Values />
-      <Products />
+      <div className={styles.body}>
+        <Nav />
+        <container className={styles.container}>
+          <video className={styles.video} src="/video.mp4"
+            autoPlay
+            loop
+            muted
+          >
+          </video>
+          <header className={styles.header}>
+            <h1 className={styles.title}>Elegância à Mesa <br /> Sabores que Encantam</h1>
+            <div>
+              <button className={styles.btn1}>Peça já</button>
+              <button className={styles.btn2}>Contate nos</button>
+            </div>
+          </header>
+        </container>
+        <Values />
+        <Products />
+      </div>
 
     </>
   );
