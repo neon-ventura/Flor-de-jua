8877046fc0099fac42b10a45d662b140f9f4a3f1
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-09-19 22:15:01 -0300

    nav

--- src/app/components/nav/Nav.jsx
+++ src/app/components/nav/Nav.jsx
@@ -12,7 +12,7 @@ export default function Nav() {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className={`${styles.nav_li} navbar-nav`}>
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link" aria-current="page" href="#">Home</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">Contato</a>
