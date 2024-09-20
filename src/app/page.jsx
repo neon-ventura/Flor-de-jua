f9687fcaa256e45f46aa39106b8b29f3584db60c
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-09-19 22:26:53 -0300

    btn zap

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,7 @@ import Products from "./components/products/Products";
 import Testimonials from "./components/testimonials/Testimonias";
 import Values from "./components/values/Values";
 import styles from "./page.module.css"
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -33,6 +34,9 @@ export default function Home() {
         <About />
         <Local />
         <Footer />
+        <a href="https://api.whatsapp.com/send/?phone=5533984550410&text&type=phone_number&app_absent=0">
+          <Image height={80} width={80} alt="Whatsapp" src={'/zap.png'} className={styles.zap} />
+        </a>
       </div>
 
     </>
