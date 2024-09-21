4dea2fb4aeb1fb1db3941b7460cfe6f11f2a83a2
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-09-21 15:26:34 -0300

    btn com link para about

--- src/app/components/about/About.jsx
+++ src/app/components/about/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
                     <h1 className={styles.title}>Sobre nós</h1>
                     <div className={styles.desc}>
                         <p className={styles.text}> No Flor de Juá Bistrô, você encontra um ambiente aconchegante e sabores únicos. Com ingredientes frescos e pratos preparados com carinho, oferecemos desde cafés especiais até refeições completas. Nosso espaço é ideal para relaxar e desfrutar de bons momentos.</p>
-                        <button className={styles.btn}>Saiba mais</button>
+                        <a href="/about"><button className={styles.btn}>Saiba mais</button></a>
                     </div>
                 </div>
             </div>
